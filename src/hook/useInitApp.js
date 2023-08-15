806fa50f08f40bb539d4f62cf92583f10d5685df
asad-ch982 <asad.fakhar982@gmail.com>
2023-08-15 16:24:37 +0500

    15-08-2023

---
+++ src/hook/useInitApp.js
@@ -0,0 +1,138 @@
+import localforage from "localforage";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import {
+  CLIENTS_KEY,
+  CLIENT_FORM_KEY,
+  COMPANY_KEY,
+  PRODUCTS_KEY,
+  PRODUCT_FORM_KEY,
+  // APP_CONTEXT,
+  INVOICE_DETAILS,
+  INVOICES_KEY,
+  INVOICE_FORM_KEY,
+  DEFAULT_INVOICE_COLOR,
+  DEFAULT_INVOICE_BG,
+} from "../constants/localKeys";
+import { useAppContext } from "../context/AppContext";
+import { updateNewClientForm, setAllClients } from "../store/clientSlice";
+import { updateCompanyData } from "../store/companySlice";
+import {
+  setAllInvoice,
+  setAllInvoiceDetailList,
+  updateNewInvoiceForm,
+} from "../store/invoiceSlice";
+import { setAllProducts, updateNewProductForm } from "../store/productSlice";
+
+const useInitApp = () => {
+  const dispatch = useDispatch();
+  const { setInitLoading } = useAppContext();
+  const setInvoices =async()=>{
+    const response = await fetch("http://localhost:3000/api/getinvoices",{
+      method:'POST',
+      headers:{
+        'content-type':'application/json'
+      }
+    })
+    const json = await response.json()
+    const data = json.data
+    
+    return data
+    
+  }
+  const setProducts =async()=>{
+    const response = await fetch("http://localhost:3000/api/getprod",{
+      method:'POST',
+      headers:{
+        'content-type':'application/json'
+      }
+    })
+    const json = await response.json()
+    const data = json
+    
+    return data
+    
+  }
+
+  const initialSetData = useCallback(async () => {
+
+    try {
+      const invoices = await setInvoices()
+      const products = await setProducts()
+      const [
+        companyData,
+        clientNewForm,
+        clients,
+        productNewForm,
+        // products,
+        // invoices,
+        invoiceDetailList,
+        invoiceNewForm,
+        defaultColor,
+        defaultBackground,
+      ] = await Promise.all([
+        localforage.getItem(COMPANY_KEY),
+        localforage.getItem(CLIENT_FORM_KEY),
+        localforage.getItem(CLIENTS_KEY),
+        localforage.getItem(PRODUCT_FORM_KEY),
+        // localforage.getItem(PRODUCTS_KEY),
+        // localforage.getItem(INVOICES_KEY),
+        localforage.getItem(INVOICE_DETAILS),
+        localforage.getItem(INVOICE_FORM_KEY),
+        localforage.getItem(DEFAULT_INVOICE_COLOR),
+        localforage.getItem(DEFAULT_INVOICE_BG),
+      ]);
+
+      if (companyData) {
+        
+        dispatch(updateCompanyData(companyData));
+      }
+
+      if (clientNewForm) {
+        dispatch(updateNewClientForm(clientNewForm));
+      }
+
+      if (clients) {
+        dispatch(setAllClients(clients));
+      }
+
+      if (productNewForm) {
+        dispatch(updateNewProductForm(productNewForm));
+      }
+
+      if (products) {
+        dispatch(setAllProducts(products));
+      }
+
+      if (invoiceNewForm) {
+        dispatch(updateNewInvoiceForm(invoiceNewForm));
+      }
+
+      if (invoices) {
+        dispatch(setAllInvoice(invoices));
+      }
+
+      if (invoiceDetailList) {
+        dispatch(setAllInvoiceDetailList(invoiceDetailList));
+      }
+
+      if (defaultColor) {
+      }
+
+      if (defaultBackground) {
+      }
+
+
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setInitLoading(false);
+    }
+  }, [dispatch, setInitLoading]);
+
+  return {
+    initialSetData,
+  };
+};
+
+export default useInitApp;
---
+++ src/store/productSlice.js
@@ -0,0 +1,154 @@
+import { createSlice } from "@reduxjs/toolkit";
+import localforage from "localforage";
+import { nanoid } from "nanoid";
+import { PRODUCTS_KEY, PRODUCT_FORM_KEY } from "../constants/localKeys";
+
+const initialState = {
+  openProductSelector: false,
+  selectedProduct: null,
+  data: [],
+  newForm: {
+    slug: nanoid(),
+    productID: "",
+    image: "",
+    name: "",
+    amount: 0,
+    // wholeSalePrice:0,
+    // desc:"",
+    // company:"",
+    // color:"",
+    // variant:"",
+    // category:"",
+    availableQty:""
+  },
+  editedID: null,
+  deletedID: null,
+};
+const setProducts =async(data)=>{
+  const response = await fetch("http://localhost:3000/api/addprod",{
+    method:'POST',
+    headers:{
+      'content-type':'application/json'
+    },
+    body: JSON.stringify({data:data})
+  })
+  const json = await response.json()
+}
+export const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    addNewProduct: (state, action) => {
+      const newDatas = [...state.data, action.payload];
+      state.data = newDatas;
+      console.log(action.payload)
+      localforage.setItem(PRODUCTS_KEY, newDatas);
+      setProducts(action.payload)
+
+      const reNewForm = {
+        slug: nanoid(),
+        productID: "",
+        image: "",
+        name: "",
+        amount: 0,
+        // wholeSalePrice:0,
+        // desc:"",
+        // company:"",
+        // color:"",
+        // variant:"",
+        // category:"",
+        availableQty:""
+      };
+
+      state.newForm = { ...reNewForm };
+      
+      localforage.setItem(PRODUCT_FORM_KEY, reNewForm);
+    },
+
+    updateNewProductForm: (state, action) => {
+      state.newForm = { ...action.payload };
+      localforage.setItem(PRODUCT_FORM_KEY, { ...state.newForm });
+    },
+
+    updateNewProductFormField: (state, action) => {
+      state.newForm[action.payload.key] = action.payload.value;
+      localforage.setItem(PRODUCT_FORM_KEY, { ...state.newForm });
+    },
+
+    setAllProducts: (state, action) => {
+      state.data = action.payload;
+    },
+
+    setDeleteId: (state, action) => {
+      state.deletedID = action.payload;
+    },
+
+    setEditedId: (state, action) => {
+      state.editedID = action.payload;
+    },
+
+    onConfirmDeletedProduct: (state, action) => {
+      const newDatas = state.data.filter(
+        (product) => product.id !== state.deletedID
+      );
+      state.data = newDatas;
+      state.deletedID = null;
+      localforage.setItem(PRODUCTS_KEY, newDatas);
+    },
+
+    onConfirmEditProduct: (state, action) => {
+      const isFindIndex = state.data.findIndex(
+        (product) => product.id === state.editedID
+      );
+      if (isFindIndex !== -1) {
+        state.data[isFindIndex] = { ...action.payload };
+      }
+      state.editedID = null;
+      localforage.setItem(PRODUCTS_KEY, [...state.data.map(product => JSON.parse(JSON.stringify(product)))]);
+    },
+
+    setOpenProductSelector: (state, action) => {
+      state.openProductSelector = action.payload;
+      if (!action.payload) {
+        state.selectedProduct = null;
+      }
+    },
+
+    setProductSelector: (state, action) => {
+      const isFindIndex = state.data.findIndex(
+        (product) => product.slug === action.payload
+      );
+      if (isFindIndex !== -1) {
+        state.selectedProduct = state.data[isFindIndex];
+      }
+    },
+  },
+});
+
+export const {
+  addNewProduct,
+  updateNewProductForm,
+  updateNewProductFormField,
+  setAllProducts,
+  setDeleteId,
+  setEditedId,
+  onConfirmDeletedProduct,
+  onConfirmEditProduct,
+  setOpenProductSelector,
+  setProductSelector,
+} = productSlice.actions;
+
+export const getAllProductSelector = (state) => state.products.data;
+
+export const getProductNewForm = (state) => state.products.newForm;
+
+export const getDeletedProductForm = (state) => state.products.deletedID;
+
+export const getEditedIdForm = (state) => state.products.editedID;
+
+export const getIsOpenProductSelector = (state) =>
+  state.products.openProductSelector;
+
+export const getSelectedProduct = (state) => state.products.selectedProduct;
+
+export default productSlice.reducer;
